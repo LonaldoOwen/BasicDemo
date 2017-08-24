71dca104bf465f3bf848628ea8063bba9b79f3d2
libowen <libowen@cct.cn>
2017-08-24 19:27:34 +0800

    work end

--- ShareExtensions/ShareExtensions/ShowUrl/ShowUrlViewController.swift
+++ ShareExtensions/ShareExtensions/ShowUrl/ShowUrlViewController.swift
@@ -14,22 +14,35 @@ class ShowUrlViewController: UIViewController {
     @IBOutlet weak var urlLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     
+    var count: Int = 0
+    var tempLabel: UILabel = {
+        let tempLabel = UILabel(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+        return tempLabel
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ShowUrlViewController: viewDidLoad")
 
         // Do any additional setup after loading the view.
         self.title = "ShowUrl"
+        
+        //
+        self.view.addSubview(tempLabel)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("ShowUrlViewController: viewWillAppear")
+        self.tempLabel.text = "count: " + String(count+1)
         
         let userDefaults = UserDefaults(suiteName: "group.com.owen.ShareExtensions")
         if (userDefaults?.bool(forKey: "has-new-share"))! {
-            let url = userDefaults?.value(forKey: "share-url")
-            self.urlLabel.text = url as? String
+            let dict = userDefaults?.value(forKey: "share-url") as! [String: String]
+            self.urlLabel.text = dict["urlString"]
+            self.nameLabel.text = dict["text"]
+            userDefaults?.removeObject(forKey: "share-url")
             userDefaults?.set(false, forKey: "has-new-share")
         }
     }
